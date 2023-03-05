22a4736358c1dc08dfa20f0c62adb966a7f2ace4
Brian Hellinger <105087652+BrianHellinger1@users.noreply.github.com>
2023-03-05 12:17:04 -0500

    Create providers.tf

---
+++ providers.tf
@@ -0,0 +1,11 @@
+#terraform providers
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.57.0"
+    }
+  }
+
+  required_version = "~> 1.3.0"
+}
